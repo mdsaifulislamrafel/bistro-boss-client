806ce14fffd55a0ef6287d5a4dbe38f470dd418d
Md Saiful Islam Rafel <mdsaifulisalmrafel@gmail.com>
2024-06-14 01:00:54 +0600

    navbar added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../pages/Shard/Footer/Footer";
+import NavBar from "../pages/Shard/NavBar/NavBar";
 
 const Main = () => {
     return (
         <div>
+            <NavBar />
             <Outlet />
             <Footer />
         </div>
