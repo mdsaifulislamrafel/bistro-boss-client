5a64b2daaa4d496c69e4d300abe5efa8aa74562b
Md Saiful Islam Rafel <mdsaifulisalmrafel@gmail.com>
2024-06-15 00:53:05 +0600

    populer menu section added

--- src/pages/Home/PopularMenu/PopularMenu.jsx
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import MenuItem from "../../Shard/MenuItem/MenuItem";
 
 const PopularMenu = () => {
+    const [menu, setMenu] = useState([]);
+    useEffect(() => {
+        fetch('/menu.json')
+            .then(res => res.json())
+            .then(data => {
+                const popularItem = data.filter(item => item.category === 'popular')
+                setMenu(popularItem);
+            })
+    }, []);
     return (
-        <section>
-            <SectionTitle subheading={'Check it out'} heading={'FROM OUR MENU'}/>
+        <section className="mb-12">
+            <SectionTitle subheading={'Check it out'} heading={'FROM OUR MENU'} />
+            <div className="grid md:grid-cols-2 gap-10">
+                {
+                    menu.map(item => <MenuItem key={item._id} item={item} />)
+                }
+            </div>
         </section>
     );
 };
