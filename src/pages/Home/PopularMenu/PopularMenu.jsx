b3d1721f8f1190607ce62a877ccaaaf0a6d87e1a
Md Saiful Islam Rafel <mdsaifulisalmrafel@gmail.com>
2024-06-15 19:11:04 +0600

    custom hooks for data loaded

--- src/pages/Home/PopularMenu/PopularMenu.jsx
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,23 +1,17 @@
-import { useEffect, useState } from "react";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import MenuItem from "../../Shard/MenuItem/MenuItem";
+import useMenu from "../../../hooks/useMenu";
 
 const PopularMenu = () => {
-    const [menu, setMenu] = useState([]);
-    useEffect(() => {
-        fetch('/menu.json')
-            .then(res => res.json())
-            .then(data => {
-                const popularItem = data.filter(item => item.category === 'popular')
-                setMenu(popularItem);
-            })
-    }, []);
+    const [menu] = useMenu();
+    const popular = menu.filter(item => item.category === 'popular')
+
     return (
         <section className="mb-12">
             <SectionTitle subheading={'Check it out'} heading={'FROM OUR MENU'} />
             <div className="grid md:grid-cols-2 gap-10">
                 {
-                    menu.map(item => <MenuItem key={item._id} item={item} />)
+                    popular.map(item => <MenuItem key={item._id} item={item} />)
                 }
             </div>
         </section>
--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,11 +1,16 @@
 import { Helmet } from 'react-helmet-async';
+import Cover from '../../Shard/Cover/Cover';
+import menuImg from '../../../assets/menu/banner3.jpg';
+
 const Menu = () => {
     return (
         <div>
             <Helmet>
                 <title>Bistro Boss | Menu</title>
             </Helmet>
-            <h2>This is a menu</h2>
+            <Cover img={menuImg} title={'OUR MENU'} />
+            
+            
         </div>
     );
 };
